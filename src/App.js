6a262b190987830a0652a4a8e43902f4f1d5f0aa
AE <austence@sbcglobal.net>
2019-10-24 11:27:41 -0700

    uses list from local state

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
