913e198ed695ba3a2cb4d3b31bf4d26ea812a055
AE <austence@sbcglobal.net>
2019-10-29 11:15:13 -0700

    splits app component into three components: App, Search, and Table components

--- src/App.js
+++ src/App.js
@@ -46,14 +46,43 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
           <input
             type="text"
-            onChange={this.onSearchChange} />
+            value={value}
+            onChange={onChange} />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -63,7 +92,7 @@ class App extends Component {
               <span>{item.points}</span>
               <span>
                   <button
-                    onClick={() => this.onDismiss(item.objectID)}
+                    onClick={() => onDismiss(item.objectID)}
                     type="button"
                   >
                     Dismiss
@@ -72,7 +101,7 @@ class App extends Component {
             </div>
         )}
       </div>
-    )
+    );
   }
 }
 
