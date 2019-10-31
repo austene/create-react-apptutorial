c9db62a17d610c29fb027b29115de9e66ecf3924
AE <austence@sbcglobal.net>
2019-10-30 18:10:58 -0700

    updates Button component to a functional stateless component

--- src/App.js
+++ src/App.js
@@ -99,23 +99,16 @@ const Table = ({ list, pattern, onDismiss }) => {
 
 }
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    )
-  }
+const Button = ({ onClick, className = '', children}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  )
 }
 
 export default App;
