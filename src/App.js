62ee80f5a9285fd462017cb5c2221890dbab10ef
AE <austence@sbcglobal.net>
2019-11-09 12:22:45 -0800

    updates fetch with axios to work across a broader list of browsers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+import axios from 'axios',
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -53,9 +54,8 @@ class App extends Component {
   };
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this.setSearchTopStories(result.data))
       .catch(error => this.setState({ error }));
   }
 
@@ -97,7 +97,7 @@ class App extends Component {
     const { searchTerm, results, searchKey, error } = this.state;
     const page = (results && results[searchKey] && results[searchKey].page) || 0;
     const list = ( results && results[searchKey] && results[searchKey].hits) || [];
-    
+
     return (
       <div className="page">
         <div className="interactions">
